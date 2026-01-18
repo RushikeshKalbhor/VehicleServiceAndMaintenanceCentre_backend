7353e5d0465d8e58b9a61fc05a628c29b26a5b9e
Rushikesh.Kalbhor <Rushikesh.Kalbhor@riomed.com>
2026-01-18 13:30:33 +0530

    solve the sonarqube issue

--- src/main/java/com/example/vehicleservice/config/AuthenticationTokensCache.java
+++ src/main/java/com/example/vehicleservice/config/AuthenticationTokensCache.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
-public class AuthenticationTokensCache {gi
+public class AuthenticationTokensCache {
 
     private final AuthenticationTokenRepository authenticationTokenRepository;
 
