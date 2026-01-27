bbd8371ffd4007b55bb482ebbf4183d0234393e4
Rushikesh.Kalbhor <Rushikesh.Kalbhor@riomed.com>
2026-01-27 10:37:05 +0530

    handle null pointer exception while creating user

--- src/main/java/com/example/vehicleservice/config/service/AuthService.java
+++ src/main/java/com/example/vehicleservice/config/service/AuthService.java
@@ -85,8 +85,10 @@ public class AuthService {
         user.setUseEmail(userRegisterJson.getUseEmail());
         user.setUseMobile(userRegisterJson.getUseMobile());
         user.setUsePassword(passwordEncoder.encode(userRegisterJson.getUsePassword()));
-        if (validationUtil.isNullOrEmpty(userRegisterJson.getUseType())) {
+        if (!validationUtil.isNullOrEmpty(userRegisterJson.getUseType())) {
             user.setUseType(userRegisterJson.getUseType());
+        } else {
+            user.setUseType("customer");
         }
         user.setUsePasswordLastModified(LocalDate.now());
         user.setUseCreated(LocalDateTime.now());
