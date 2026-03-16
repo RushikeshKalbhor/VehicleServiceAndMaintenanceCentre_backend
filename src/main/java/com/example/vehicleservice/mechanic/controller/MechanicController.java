b378d63c6757876d698c56170b3c07c8d26ce466
Rushikesh.Kalbhor <Rushikesh.Kalbhor@riomed.com>
2026-03-16 21:34:44 +0530

    remove unused import

--- src/main/java/com/example/vehicleservice/mechanic/controller/MechanicController.java
+++ src/main/java/com/example/vehicleservice/mechanic/controller/MechanicController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
